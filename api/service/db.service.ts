a6770e1c637c374e7f349093b9e1758d1135b41e
Victor Marotta <victordossantosmarotta@gmail.com>
2023-04-05 06:48:54 -0300

    fix: fixed column type in db.service

--- api/service/db.service.ts
+++ api/service/db.service.ts
@@ -5,25 +5,12 @@ const db = connection();
 
 export async function searchService(keywords: string) {
 
-  // CREATE VIEW webgis.search_table AS
-  // SELECT 
-  //   'rrcm_principal_p' || '.' || id::text AS id, -- substituir rrcm_principal_p pelo nome da tabela
-  //   'rrcm_principal_p' AS table_name, -- substituir rrcm_principal_p pelo nome da tabela
-  //   endereco || ' ' || descricao_itinerario AS search_body, -- substituir pelos campos utilizados na busca
-  //   json_build_object(
-  //     'geometry', ST_asgeojson(geom),
-  //     'id', 'rrcm_principal_p' || '.' || id::text, -- substituir rrcm_principal_p pelo nome da tabela e o id pela chave primaria
-  //     'properties', row_to_json(d.*),
-  //     'type', 'Feature'
-  //   ) features
-  // FROM dados.rrcm_principal_p d;
-
   keywords = keywords.split(' ').join(' & ')
 
   consoleLog(`db: Querying for keywords: ${keywords}`)
 
   let query = `SELECT json_build_object(
-    'features', json_agg(search_table.features),
+    'features', json_agg(search_table.features::json),
     'type', 'FeatureCollection'
   ) As features
   FROM webgis.search_table 
