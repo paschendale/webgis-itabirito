2f6d18dd8ac1f04aa3a85f6db54b52044be95a5b
Victor Marotta <victordossantosmarotta@gmail.com>
2023-05-03 07:23:53 -0300

    refact: changed map page to openlayers

--- app/src/pages/webgis/index.tsx
+++ app/src/pages/webgis/index.tsx
@@ -1,23 +1,36 @@
-import { MapContainer, useMapEvents, WMSTileLayer } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css';
+// import { MapContainer, useMapEvents, WMSTileLayer } from 'react-leaflet'
+// import { LeafletMouseEvent } from 'leaflet';
+// import 'leaflet/dist/leaflet.css';
+// import SelectedFeatures from './components/selectedFeatures';
 import './styles.css'
+import { ButtonsContainer, Container, LeftSidePanel, LeftSidePanelSwitcher, MapContainer, MiddlePanel, RightSidePanel, RightSidePanelSwitcher, Version } from './styles';
 import { useEffect, useRef, useState } from "react"
-import SearchBox from '../searchbox';
-import InfoPanel from '../info-panel';
-import { ButtonsContainer, Container, LeftSidePanel, LeftSidePanelSwitcher, MiddlePanel, RightSidePanel, RightSidePanelSwitcher, Version } from './styles';
+import SearchBox from '../../modules/searchbox';
+import InfoPanel from '../../modules/info-panel';
 import { api } from '../../services/api';
-import { LeafletMouseEvent } from 'leaflet';
 import { generateQueryParams, toastError } from '../../utils';
 import { FaCaretLeft, FaCaretRight, FaGithub, FaRulerCombined, FaStreetView } from 'react-icons/fa';
-import MapButton from '../../components/mapButton';
 import PanoramicViewer from '../../components/panoramic-viewer';
 import { useLocation } from 'react-router-dom';
 import pj from "./../../../package.json"
 import 'proj4';
 import 'proj4leaflet';
-import SelectedFeatures from './components/selectedFeatures';
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
+import MapButton from '../../components/mapButton';
+
+import OlMap from 'ol/Map';
+import OlView from 'ol/View';
+import OlLayerTile from 'ol/layer/Tile';
+import OlSourceOsm from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile.js';
+import TileWMS from 'ol/source/TileWMS.js';
+import { fromLonLat } from 'ol/proj';
+
+import 'ol/ol.css';
+import {
+  MapComponent
+} from '@terrestris/react-geo';
 
 interface Layer {
   '@_queryable': string;
@@ -40,6 +53,24 @@ function Map() {
   const[displayRightSidePanel,setdisplayRightSidePanel] = useState(false)
   const[features,setFeatures] = useState<any>()
   const[isLoadingInfoPanel,setIsLoadingInfoPanel] = useState(false)
+
+  const layer = new OlLayerTile({
+    source: new OlSourceOsm()
+  });
+
+  const map = new OlMap({
+    view: new OlView({
+      center: fromLonLat([-43.80376, -20.25554]),
+      zoom: 14,
+    })
+  });
+
+  useEffect(() => {
+    if(layers && layerOrder) {
+
+      makeLayers(layers,layerOrder)
+    }
+  },[layers])
 
   useEffect(() => {
 
@@ -76,6 +107,24 @@ function Map() {
     .catch((error: any) => null)
   },[projectId])
 
+  function switchLeftPanel() {
+
+    setdisplayLeftSidePanel(!displayLeftSidePanel)
+  }
+
+  function switchRightPanel() {
+
+    setdisplayRightSidePanel(!displayRightSidePanel)
+  }
+  
+  function measureTool(e: any) {
+    console.log('mediremos em breve')
+  }
+
+  function streetView(e: any) {
+    console.log('veremos em breve')
+  }
+
   function makeLayers(layers: any, order: string) {
 
     const content = order.split(',').map(
@@ -97,111 +146,20 @@ function Map() {
       baseLayer = false
     }
 
-    return (
-      <WMSTileLayer
-        key={layer.Title}
-        id={layer.Title}
-        url={`/api/map/${projectId}`}
-        layers={layer.Name}
-        format={baseLayer ? 'image/jpeg' : 'image/png'}
-        transparent={!baseLayer}
-        maxZoom={30}
-        maxNativeZoom={30}
-      />
-    )
+    var olLayer = new TileLayer({
+      source: new TileWMS({
+        url: `/api/map/${projectId}`,
+        params: {
+          'layers': layer.Name, 
+          'format': baseLayer ? 'image/jpeg' : 'image/png',
+          'maxZoom': 30,
+          'transparent': !baseLayer
+        }
+      }),
+    })
+
+    map.addLayer(olLayer)
   }
-
-  async function getFeatureInfo(map: any, e: LeafletMouseEvent) {
-
-      var point = map.latLngToContainerPoint(e.latlng)
-      var size = map.getSize()
-      var crs = map.options.crs
-      var sw = crs?.project(map.getBounds().getSouthWest())
-      var ne = crs?.project(map.getBounds().getNorthEast())
-      var lyrs = layers?.filter(e => e['@_queryable'] === '1').map(e => e.Name)
-
-      var params = {
-        request: 'GetFeatureInfo',
-        service: 'WMS',
-        version: '1.1.0',
-        srs: 'EPSG:3857',
-        x: JSON.stringify(point.x),
-        y: JSON.stringify(point.y),
-        bbox: JSON.stringify(sw?.x) + ',' + JSON.stringify(sw?.y) + ',' + JSON.stringify(ne?.x) + ',' + JSON.stringify(ne?.y),
-        height: JSON.stringify(size.y),
-        width: JSON.stringify(size.x),
-        layers: lyrs?.join(','),
-        query_layers: lyrs?.join(','),
-        info_format: 'application/json',
-        with_geometry: 'true',
-        feature_count: '50000'
-    };
-
-    var queryParams = generateQueryParams(params)
-
-    try {
-      
-      var response = await api.get(`/map/info/${projectId}?${queryParams}`)
-  
-      var featureInfo = response.data
-  
-      setIsLoadingInfoPanel(false)   
-      setFeatures(featureInfo.features)
-      return featureInfo
-    } catch (error: any) {
-      
-      setIsLoadingInfoPanel(false)   
-      setFeatures([])
-      
-      if (error.response.status === 401) {
-          
-        toastError('O usuário não possui credenciais válidas para realizar esta operação.')
-      } else {
-
-        toastError('Ocorreu um erro ao tentar obter as feições no local selecionado.')
-      }
-
-      throw error
-    }
-
-  }
-
-  function switchLeftPanel() {
-
-    setdisplayLeftSidePanel(!displayLeftSidePanel)
-  }
-
-  function switchRightPanel() {
-
-    setdisplayRightSidePanel(!displayRightSidePanel)
-  }
-
-  function measureTool(e: any) {
-    console.log('mediremos em breve')
-  }
-
-  function streetView(e: any) {
-    console.log('veremos em breve')
-  }
-
-  const MapHandlers = () => {
-    
-    const map = useMapEvents({
-        click(e: any) {
-
-          if(!displayLeftSidePanel) {
-            setdisplayLeftSidePanel(true)
-          }
-          setIsLoadingInfoPanel(true) 
-          getFeatureInfo(this,e)
-        },
-        zoomend: () => {
-          console.log(map,map.getZoom());
-        },
-    });
-
-    return null;
-  };
 
   return ( 
     <>
@@ -214,21 +172,28 @@ function Map() {
           />
         </LeftSidePanel>
         <MiddlePanel>
+          
+          <LeftSidePanelSwitcher onClick={switchLeftPanel}>
+              {(displayLeftSidePanel)? (<FaCaretLeft/>) : (<FaCaretRight/> )}
+          </LeftSidePanelSwitcher>
           <ButtonsContainer>
             <MapButton onClick={measureTool}><FaRulerCombined/></MapButton>
             <MapButton onClick={streetView}>
               <FaStreetView />
             </MapButton>
           </ButtonsContainer>
-          <LeftSidePanelSwitcher onClick={switchLeftPanel}>
-              {(displayLeftSidePanel)? (<FaCaretLeft/>) : (<FaCaretRight/> )}
-          </LeftSidePanelSwitcher>
           <SearchBox 
             setFeatures={setFeatures}
             setLoading={setIsLoadingInfoPanel}
             setDisplayLeftSidePanel={setdisplayLeftSidePanel}
             layers={layers}
           />
+          <MapContainer id="map-container">
+            <MapComponent id="map-component"
+              map={map}
+            />
+          </MapContainer>
+          {/* 
           <MapContainer 
             center={[-20.25554, -43.80376]} 
             zoom={17} 
@@ -245,7 +210,7 @@ function Map() {
             <SelectedFeatures
               features={features}
             />   
-          </MapContainer>
+          </MapContainer> */}
           <Version>
             <div style={{padding: 4}}>
               webgis-itabirito:v.{pj.version} <a style={{color: 'inherit'}} href='https://github.com/paschendale/webgis-itabirito' target={'_blank'} rel="noreferrer"><span style={{color: 'inherit'}}><FaGithub/></span></a>
---
+++ app/src/pages/webgis/styles.ts
@@ -0,0 +1,201 @@
+import styled from "styled-components";
+import denim from "./../../assets/denim.webp";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  z-index: 1;
+  display: flex;
+  flex-direction: row;
+  overflow-y: hidden;
+`;
+
+type PanelProps = {
+  display: boolean;
+}
+
+export const LeftSidePanel = styled.div<PanelProps>`
+  display: ${(props: PanelProps) => ((props.display)? 'auto' : 'none')};
+  z-index: 900;
+  box-shadow: 6px 2px 5px 0px rgba(0,0,0,0.5);
+  -webkit-box-shadow: 6px 2px 5px 0px rgba(0,0,0,0.5);
+  -moz-box-shadow: 6px 2px 5px 0px rgba(0,0,0,0.5);
+  transition: all 0.5s;
+`;
+
+export const RightSidePanel = styled.div<PanelProps>`
+  display: ${(props: PanelProps) => ((props.display)? 'auto' : 'none')};
+  width: 500px;
+  min-width: 500px;
+  z-index: 900;
+  box-shadow: -6px 2px 5px 0px rgba(0,0,0,0.5);
+  -webkit-box-shadow: -6px 2px 5px 0px rgba(0,0,0,0.5);
+  -moz-box-shadow: -6px 2px 5px 0px rgba(0,0,0,0.5);
+
+  @media (prefers-color-scheme: dark) {
+    color: rgb(180,180,180);
+    background-image: linear-gradient(rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0.50)), url(${denim});
+  }
+`;
+
+export const MiddlePanel = styled.div`
+  width: 100%;
+  min-width: 30vw;
+  height: 100%;
+`;
+
+export const LeftSidePanelSwitcher = styled.div`
+  cursor: pointer;
+  z-index: 1000;
+  position: absolute;
+  width: 14px;
+  height: 50px;
+  bottom: 10px;
+  border: none;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  box-shadow: 6px 2px 5px 0px rgba(0,0,0,0.5);
+  -webkit-box-shadow: 6px 2px 5px 0px rgba(0,0,0,0.5);
+  -moz-box-shadow: 6px 2px 5px 0px rgba(0,0,0,0.5);
+  background-color: whitesmoke;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (prefers-color-scheme: dark) {
+    color: rgb(180,180,180);
+    border: none;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0.50)), url(${denim});
+  }
+`;
+
+export const RightSidePanelSwitcher = styled.div`
+  cursor: pointer;
+  z-index: 1000;
+  position: relative;
+  width: 14px;
+  height: 50px;
+  bottom: 62px;
+  margin-left: calc(100% - 14px);
+  border: none;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  box-shadow: -6px 2px 5px 0px rgba(0,0,0,0.5);
+  -webkit-box-shadow: -6px 2px 5px 0px rgba(0,0,0,0.5);
+  -moz-box-shadow: -6px 2px 5px 0px rgba(0,0,0,0.5);
+  background-color: whitesmoke;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (prefers-color-scheme: dark) {
+    color: rgb(180,180,180);
+    background-image: linear-gradient(rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0.50)), url(${denim});
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  margin-top: 80px;
+  margin-left: 10px;
+  z-index: 1000;
+  position: absolute;
+  background: whitesmoke;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  box-shadow: 2px 2px 2px 0px rgba(0,0,0,0.5);
+  -webkit-box-shadow: 2px 2px 2px 0px rgba(0,0,0,0.5);
+  -moz-box-shadow: 2px 2px 2px 0px rgba(0,0,0,0.5);
+  border-radius: 4px;
+
+  @media (prefers-color-scheme: dark) {
+    color: rgb(180,180,180);
+    background-image: linear-gradient(rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0.50)), url(${denim});
+  }
+`;
+
+export const Placeholder = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: red;
+`
+export const Version = styled.div`
+  z-index: 10000;
+  position: absolute;
+  bottom: 5px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(255,255,255,0.5);
+  font-size: xx-small;
+  border-radius: 4px;
+  opacity: 0.5;  
+  
+  @media (prefers-color-scheme: dark) {
+    color: rgb(180,180,180);
+    background-color: rgb(0,0,0,0.8);
+  }
+`
+
+export const MapContainer = styled.div `
+  width: 100%;
+  height: 100%;
+  
+  @media (prefers-color-scheme: dark) {
+    color: rgb(180,180,180);
+    background-image: linear-gradient(rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0.50)), url(${denim});
+  }
+
+  .map {
+    height: 100%;
+  }
+
+  .ol-control {
+    color: black;
+    background-color: whitesmoke;
+    border: none;    
+    box-shadow: rgba(0, 0, 0, 0.5) 2px 2px 2px 0px;
+    border-radius: 4px;
+    padding: 0;
+
+    button {
+      cursor: pointer;
+      pointer-events: all;      
+      background-color: whitesmoke;
+      color: black;
+      border-radius: 4px;
+
+      @media (prefers-color-scheme: dark) {
+        color: rgb(180,180,180);
+        background-image: linear-gradient(rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0.50)), url(${denim});
+      }
+    }
+
+    @media (prefers-color-scheme: dark) {
+      color: rgb(180,180,180);
+      background-image: linear-gradient(rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0.50)), url(${denim});
+    }
+  }
+
+  .ol-zoom-out {   
+    margin: 0;
+    border: 1px solid rgba(0,0,0,0.2);  
+    border-top: none;   
+    width: 32px;
+    height: 32px;
+  }
+
+  .ol-zoom-in {    
+    margin: 0;
+    border: 1px solid rgba(0,0,0,0.2);    
+    width: 32px;
+    height: 32px;
+  }
+
+  .ol-attribution {
+    display: none;
+  }
+`
